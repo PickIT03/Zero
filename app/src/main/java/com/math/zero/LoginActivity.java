9cc582e1ded4de06e32b09cb4ee3703c1c1fc56c
PickIT03 <pickit03@gmail.com>
2022-07-11 21:37:29 +0530

    UI update

--- app/src/main/java/com/math/zero/LoginActivity.java
+++ app/src/main/java/com/math/zero/LoginActivity.java
@@ -5,19 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,WindowManager.LayoutParams.FLAG_SECURE);
         setContentView(R.layout.activity_login);
         Button button=findViewById(R.id.login);
         button.setOnClickListener(view -> {
             startActivity(new Intent(LoginActivity.this,HomePage.class));
             finish();
         });
+        LinearLayout layout=findViewById(R.id.signup);
+        layout.setOnClickListener(view -> {
+            startActivity(new Intent(LoginActivity.this,SignUpActivity.class));
+            finish();
+        });
+    }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
--- app/src/main/java/com/math/zero/SignUpActivity.java
+++ app/src/main/java/com/math/zero/SignUpActivity.java
@@ -2,7 +2,16 @@ package com.math.zero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +19,25 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ImageView back=findViewById(R.id.back);
+        LinearLayout linearLayout=findViewById(R.id.login);
+        back.setOnClickListener(view -> {startActivity(new Intent(SignUpActivity.this,LoginActivity.class));finish();});
+        linearLayout.setOnClickListener(view -> {startActivity(new Intent(SignUpActivity.this,LoginActivity.class));finish();});
+        String[] department=getResources().getStringArray(R.array.department);
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<>(this,R.layout.dropdown_list,department);
+        AutoCompleteTextView autoCompleteTextView=findViewById(R.id.autoCompleteTextView);
+        autoCompleteTextView.setAdapter(arrayAdapter);
+        Button signup=findViewById(R.id.signup);
+        signup.setOnClickListener(view -> {
+            String dept=autoCompleteTextView.getText().toString().trim();
+            System.out.println(dept);
+            Toast.makeText(this, dept, Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(SignUpActivity.this,LoginActivity.class));
+        finish();
     }
 }
