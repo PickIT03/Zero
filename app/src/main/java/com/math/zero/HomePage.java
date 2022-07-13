c6e9b17e72b4751bab2790cb543e0f9c0eba5241
PickIT03 <pickit03@gmail.com>
2022-07-13 22:30:11 +0530

    Home UI update

--- app/src/main/java/com/math/zero/HomePage.java
+++ app/src/main/java/com/math/zero/HomePage.java
@@ -2,7 +2,9 @@ package com.math.zero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        LinearLayout logout=findViewById(R.id.logout_linear);
+        logout.setOnClickListener(view -> {startActivity(new Intent(HomePage.this,LoginActivity.class));finish();});
     }
 }
